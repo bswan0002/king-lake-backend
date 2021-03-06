c9a6af5a8aeee665a1df8fbe3302947e0466cff4
bswan0002 <bswan0002@gmail.com>
2021-03-05 21:27:57 -0800

    clean up order serializers

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,3 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :pickup_date, :prepared, :paid_for, :picked_up, :items
+  attributes :id, :user_id, :created_at, :pickup_date, :prepared, :paid_for, :picked_up, :items
 end
